5017d4e7f0010db5e8c4ac227a506717ce44fcfa
AdamczukPawel <104322297+AdamczukPawel@users.noreply.github.com>
2023-03-25 20:59:21 +0100

    Fix

--- src/pages/phonebook.js
+++ src/pages/phonebook.js
@@ -4,6 +4,7 @@ import { fetchContacts } from 'redux/contacts/contacts.thunk';
 import { ContactForm } from 'components/ContactForm/ContactForm';
 import { Filter } from 'components/Filter/Filter';
 import { ContactList } from 'components/ContactList/ContactList';
+import { Card } from '@chakra-ui/react';
 
 export const Phonebook = () => {
   const dispatch = useDispatch();
@@ -13,10 +14,16 @@ export const Phonebook = () => {
   }, [dispatch]);
 
   return (
-    <>
+    <Card
+      display="flex"
+      align="center"
+      marginRight="auto"
+      marginLeft="auto"
+      padding="50px"
+    >
       <ContactForm />
       <Filter />
       <ContactList />
-    </>
+    </Card>
   );
 };
